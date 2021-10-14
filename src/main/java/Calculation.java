5dfd3064217f84f5d2876b0a592122a0bcf2e189
AminaIsazada <amina.isazade.std@bhos.edu.az>
2021-10-14 23:36:53 +0400

    Adding Lab 4

---
+++ src/main/java/Calculation.java
@@ -0,0 +1,51 @@
+public class Calculation {
+    int result;
+    //Method for addition - 1
+    public int add(int num1, int num2){
+        result = num1+num2;
+        return result;
+    }
+    //Method for subtraction - 2
+    public int subtract(int num1, int num2){
+        result = num1-num2;
+        return result;
+    }
+    //Method for multiplication - 3
+    public int multiply(int num1, int num2){
+        result = num1*num2;
+        return result;
+    }
+    //Method for division - 4
+    public float divide(int num1, int num2){
+        result = num1/num2;
+        return result;
+    }
+    //Method for even or not - 5
+    public boolean even(int num) {
+        return num % 2 == 0;
+    }
+    //Method for odd or not - 6
+    public boolean odd(int a) {
+        return a % 2 != 0;
+    }
+    //Method for mode - 7
+    public float mode(int num1, int num2) {
+        result = num1 % num2;
+        return result;
+    }
+    //Method for increasing - 8
+    public void increase(int num) {
+        result = num++;
+    }
+
+    //Method for decreasing - 9
+    public void decrease(int num) {
+        result = num--;
+    }
+    //Method for logarithm - 10
+    public double logarithm(double num) {
+        return Math.log(num);
+    }
+
+
+}
